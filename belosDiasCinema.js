164de4002fe650d1b20265246bccbbfba907ff0e
RafaelPatricio87 <87397552+RafaelPatricio87@users.noreply.github.com>
2022-11-24 02:59:20 -0300

    subindo exercicios

--- belosDiasCinema.js
+++ belosDiasCinema.js
@@ -1,6 +1,19 @@
 function diaDeCinema(i,j,k){
-   for(let index = 0 ; index < 10 ; index++){
-    console.log(index)
+   let reverse = 0
+   let total = 0
+
+  for(let index = i; index <= j; index++){
+    reverse = Number(String(index).split('').reverse().join(''))
+   if(Number.isInteger(Math.abs(index - reverse) / k) === true){
+      total += 1
    }
+  }
+  return total
 }
 console.log(diaDeCinema(20,23,6))
+
+
+
+// console.log(reverse.toString().split('').reverse().join('')) 
+
+
