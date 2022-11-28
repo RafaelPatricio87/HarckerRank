1a73a6b364412769012e791cd8740381b12298da
RafaelPatricio87 <87397552+RafaelPatricio87@users.noreply.github.com>
2022-11-28 05:16:54 -0300

    hackerrank exercicio

---
+++ viralAdvertising.js
@@ -0,0 +1,14 @@
+function viralAdvertising(n) {
+
+    let shared = 5, like = 0, totalLikes = 0;
+
+    for (let i = 1; i <= n; i++) {
+        // console.log(i)
+        like = Math.floor(shared / 2);
+        shared = like * 3;
+        totalLikes += like;
+       
+    }
+    return totalLikes;
+}
+console.log(viralAdvertising(5))
