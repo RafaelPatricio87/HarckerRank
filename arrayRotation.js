a46d48936bdf6c887a1d2075fab3c7d67924dc85
RafaelPatricio87 <87397552+RafaelPatricio87@users.noreply.github.com>
2022-12-10 04:32:12 -0300

    exercicio do hanckerrank

--- arrayRotation.js
+++ arrayRotation.js
@@ -1,5 +1,15 @@
 function arrayRotation(a,k,queries){
  
+for(let index = 0; index < k; index++){
+   const ultimoArray = a.pop()
+a.unshift(ultimoArray) 
+
+}  
+let ar = []  
+for(let index2 = 0; index2 < queries.length; index2++){
+   ar.push(a[queries[index2]]) 
+}
+return ar
 
 }
-console.log(arrayRotation([1,2,3] ,2 ,[0,1,2]))
+console.log(arrayRotation([3,4,5] ,2 ,[1,2]))
---
+++ saltosNuvens.js
@@ -0,0 +1,9 @@
+function saltandoClouds(c,k){
+    const e = 100
+    for(let i = 0; i < c.length; i++){
+        console.log(c[i])
+    }
+    console.log(c)
+}
+
+console.log(saltandoClouds([0, 0, 1, 0, 0, 1, 1, 0],2))
