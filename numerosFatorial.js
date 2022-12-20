02c87a1e6ac925ab0ef0b36f555fa9a86ff474c4
RafaelPatricio87 <87397552+RafaelPatricio87@users.noreply.github.com>
2022-12-20 01:23:35 -0300

    Create numerosFatorial.js

---
+++ numerosFatorial.js
@@ -0,0 +1,8 @@
+function extraLongFactorials(n){
+  n = BigInt(n)
+  let num = BigInt(1)
+  for(let index = n ; index > 1; index -= 1){
+    console.log(n)
+  }
+}
+console.log(extraLongFactorials(25))
