46e2315cca42930eb16709f5bd25cded6b161079
RafaelPatricio87 <87397552+RafaelPatricio87@users.noreply.github.com>
2022-12-24 05:03:05 -0300

    exercicio do hackerank

--- bibliotecaMulta.js
+++ bibliotecaMulta.js
@@ -1,30 +1,19 @@
-function libraryFine(d1,m1,y1, d2,m2,y2){
-   let multa = 0
-   if(y1 > y2 && m1 >= m2 && d1 >= d2){
-     multa = 10000
-   }
+
+function libraryFine(d1,m1,y1,d2,m2,y2){
+  if(y1 < y2){
+    return 0
+  }
+  if(y1 == y2 && m1 < m2){
+    return 0
+  }else if(y1 == y2 && m1 == m2 && d1 <= d2){
+    return 0
+  }else if(y1 == y2 && m1 == m2 && d1 > d2){
+    return 15 * (d1 - d2)
+  }else if(y1 == y2 && m1 > m2 ){
+    return 500 * (m1 - m2)
+  }else {
+    return 10000
+  }
 }
-console.log(libraryFine(2 ,6 ,2014,7 ,6 ,2014))
-// apos o dia 15 hackos
-// apos o mes 500 hackos
-// apos o ano 10000
+console.log(libraryFine(1, 1 ,2015,31 ,12 ,2014))
 
-
-// const dia = d1- d2
-//    const mes = Math.abs(m1  -m2)
-//    const ano = Math.abs(y1 - y2)
-//    let multa = 0
-//     if(ano > 0){
-//         if(m1 > m2){
-//          return  multa = 10000
-//         }else if(m1 == m2 && d1 > d2){
-//             return multa = 10000
-//         }
-       
-//     }
-//    if(dia > 0 && mes == 0){
-//     multa = dia * 15
-//    }else if(mes !== 0){
-//     multa = mes * 500
-//    }
-//    return multa
