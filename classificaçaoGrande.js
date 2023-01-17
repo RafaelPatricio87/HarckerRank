530d1460fcfce949edd169950f3df91dc7bf61ca
RafaelPatricio87 <87397552+RafaelPatricio87@users.noreply.github.com>
2023-01-17 00:44:39 -0300

    Update classificaçaoGrande.js

--- classificaçaoGrande.js
+++ classificaçaoGrande.js
@@ -1,5 +1,13 @@
 function bigSorting(unsorted){
-   const unsorteds = BigInt(unsorted)
-   console.log(typeof unsorted)
+//   unsorted.sort((a,b )=> a - b)
+//   unsorted.sort((a,b) => (BigInt(a) > BigInt(b) ? 1: - 1))
+  unsorted.sort((a,b) => {
+   if(a.length === b.length){
+      return a > b ? 1: -1 ;
+   }else {
+      return a.length - b.length
+   }
+  })
+  return unsorted
 }
-console.log(['1','200','150','3'])
+console.log(bigSorting(['1','2','100','12303479849857341718340192371 ','3084193741082937 ','3084193741082938 ','111','200']))
